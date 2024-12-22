5bbed97a763bbb7ad2067a70e8b00cc58fcf4801
“Loic <kouedelo37@gmail.com>
2024-12-22 14:08:06 -0500

    Remove alumni details link

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import MainLayout from "./components/MainLayout";
 import AuthLayout from "./components/AuthLayout";
-import AlumniDetails from './components/AlumniDetails';
 import "./App.css";
 
 function App() {
@@ -32,14 +31,6 @@ function App() {
           <AuthLayout>
             <Signup />
           </AuthLayout>
-        }
-      />
-      <Route
-        path="/alumni-details"
-        element={
-          <MainLayout>
-            <AlumniDetails />
-          </MainLayout>
         }
       />
     </Routes>
