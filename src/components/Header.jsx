0b5dee35011b79c1e80cac1bac326669095f1dfd
“Loic <kouedelo37@gmail.com>
2025-01-05 20:21:47 -0500

    #32 refactored nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,41 +14,54 @@ const Header = () => {
 
   return (
     <AppBar position="static" elevation={0} color="primary">
-      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <div className="flex items-center">
-          <Typography
-            variant="h6"
-            style={{ fontWeight: 700 }}
-            color="secondary"
-            mr={2}
-          >
-            PKFOKAM ALUMNI
-          </Typography>
-          <nav>
-            <List orientation="horizontal" sx={{ display: "flex" }}>
-              {menu.map((item, index) => (
-                <ListItem key={index}>
-                  <Typography
-                    variant="body1"
-                    color="secondary"
-                    sx={{
-                      cursor: "pointer",
-                      "&:hover": {
-                        color: "white",
-                      },
-                    }}
-                  >
-                    {item}
-                  </Typography>
-                </ListItem>
-              ))}
-            </List>
-          </nav>
+      <div className="flex">
+        <div className="flex items-center pl-6">
+          <Link to={"/"}>
+            <Typography
+              variant="h6"
+              style={{ fontWeight: 700 }}
+              color="secondary"
+              mr={2}
+            >
+              PK FOKAM | ALUMNI
+            </Typography>
+          </Link>
         </div>
-        <Button component={Link} to="/login" variant="contained">
-          Login
-        </Button>
-      </Toolbar>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            flexGrow: 1,
+          }}
+        >
+          <div className="flex items-center justify-end">
+            <nav>
+              <List orientation="horizontal" sx={{ display: "flex" }}>
+                {menu.map((item, index) => (
+                  <ListItem key={index}>
+                    <Typography
+                      variant="body1"
+                      color="secondary"
+                      sx={{
+                        cursor: "pointer",
+                        "&:hover": {
+                          color: "white",
+                        },
+                        fontWeight: 600,
+                      }}
+                    >
+                      {item}
+                    </Typography>
+                  </ListItem>
+                ))}
+              </List>
+            </nav>
+          </div>
+          <Button component={Link} to="/login" variant="contained">
+            Login
+          </Button>
+        </Toolbar>
+      </div>
     </AppBar>
   );
 };
